fa3e4b8636e88f87fbdfc58012acb75f1bf73b35
oBoii <fabian.denoodt@student.odisee.be>
2019-09-25 14:26:54 +0200

    course notes

---
+++ 01_hello_world.py
@@ -0,0 +1,20 @@
+print('hi')
+print(type(23.0))
+print("a"*5)
+print(str(4))
+
+print("{:.2f}".format(30))
+lis = [1, 2, 3]
+print(lis)
+
+a = {
+        'a': 'r1'
+    }
+a['a'] = 9
+a['b'] = 99
+print(a)
+
+
+"""
+fdsakj;l
+"""
