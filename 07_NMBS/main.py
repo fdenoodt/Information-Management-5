bec1e7a7fecccb0638b1c1acc7b20ef96f9d714f
oBoii <fabian.denoodt@student.odisee.be>
2019-11-06 17:50:52 +0100

    f

--- 07_NMBS/main.py
+++ 07_NMBS/main.py
@@ -3,6 +3,35 @@ from trains import TrainApi
 import datetime
 import time
 
+
+def display_vias(conn):
+    stops = ''
+    if 'via' in conn['vias']:
+        viaid = 1
+        for via in conn['vias']['via']:
+            stops += ', via: ('+via['direction']['name'] + \
+                ') - ' + str(via['station'])
+            viaid += 1
+        print(conn['departure']['station'] + stops + ' ('+conn['vias']['via'][int(conn['vias']
+                                                                                  ['number'])-1]['departure']['direction']['name'] + ')' + ' - ' + conn['arrival']['station'])
+    else:
+        print(conn['id']+': '+conn['departure']['station'] + ' ('+conn['departure']
+              ['direction']['name'] + ')' + ' - ' + conn['arrival']['station'])
+
+
+def display_connection(conn):
+    print("******************")
+    message = conn['id']
+    message = message + ' : ' + str(conn['departure']['time'])
+    message = message + ' - ' + str(conn['arrival']['time'])
+
+    print(message)
+    display_vias(conn)
+
+    print("Duration: " + str(int(conn['duration']) / 60) + " minutes")
+    print("\n")
+
+
 train_api = TrainApi()
 # 1 ask for the data (nu gebruiken we test data)
 # indien de verwachte trein te laat is en er geen vroegere is dan wil je misschien een latere trein dan je gewilde arrival_time. (geeft ons de optie om een latere trein te kunnen kiezen)
@@ -10,76 +39,60 @@ max_later = 0  # in min
 # maximum aantal minuten dat de trein te laat mag zijn
 max_delay = 15  # in min
 
-#
 alarm = 100
 
 starting_location = 'Gent-Sint-Pieters'
-# starting_location = 'Brussel-Noord'
-# end_location = 'Arlon'
 end_location = 'Dilbeek'
-# end_location = 'Brussel-Noord'
 date = '061119'
-# arrival time versimpelen (als je max later = 100 en je arrival_time = 2300 dan moet je dus 00000 en niet 2400)   andere vb ook (dus als je minuten toevoeg mag je niet  1260 of 1270)
-arrival_time = str(int('1420') + max_later)
+
+str_last_available_arrival_time = str(int('1420') + max_later)
+last_available_arrival_DATE_time = datetime.datetime(
+    2000+int(date[4:6]),
+    int(date[2:4]),
+    int(date[0:2]),
+    int(str_last_available_arrival_time[0:2]),
+    int(str_last_available_arrival_time[3:5]))
 
 connections = train_api.get_connections(
-    starting_location, end_location, date, arrival_time)
+    starting_location, end_location, date, '1450')
 
 # 2. print all the available routes
+print("\n")
+print('Available routes:')
 for conn in connections:
-    print(str(conn['departure']['time']) +
-          ' - ' + str(conn['arrival']['time']))
-    stops = str('')
-    if 'vias' in conn:
-        if 'via' in conn['vias']:
-            viaid = 1
-            for via in conn['vias']['via']:
-                stops += ' ('+via['direction']['name'] + \
-                    ') - ' + str(via['station'])
-                viaid += 1
-        # print(conn['id']+': '+conn['departure']['station'] + stops + ' ('+conn['vias']['via'][int(conn['vias']
-        #                                                                                           ['number'])-1]['departure']['direction']['name'] + ')' + ' - ' + conn['arrival']['station'])
-    else:
-        print(conn['id']+': '+conn['departure']['station'] + ' ('+conn['departure']
-              ['direction']['name'] + ')' + ' - ' + conn['arrival']['station'])
-    # id = conn['vias']['number']
-    # '('++')'          [1]['departure']['direction']['name']
-    print("duration: " + conn['duration'])
+    display_connection(conn)
+
 
 print('\n')
 print('Choose a route:')
 
-# 3
 originalid = int(input())
 selected_connection = connections[originalid]
 
-print(selected_connection)
+print("\n")
+print("Selected route:")
+display_connection(selected_connection)
 
 # 4
 while True:
     try:
+        print("Verifying connection...")
+        is_on_time = train_api.verify_delay(
+            selected_connection, last_available_arrival_DATE_time)
+        print("The currently selected train is on time: " + str(is_on_time))
 
-        is_on_time = train_api.verify_delay(selected_connection, date)
-        print("Is on time" + str(is_on_time))
         if is_on_time == False:
-            print("connection has changed to something different")
             # Pick new connection
             connections = train_api.get_connections(
-                starting_location, end_location, date, arrival_time)
+                starting_location, end_location, date, str_last_available_arrival_time)
 
             selected_connection = connections[0]
-        else:
-            print("connection on time")
+
+            print("A new route/connection has been selected:")
+            display_connection(selected_connection)
 
         time.sleep(10)
     except:
-        print("err, whatever")
+        print("Oops something went wrong.")
 
-'''
-1. ask for starting location, end location, expected arrival
-2. provide list of routes to take
-3. user chooses a route
-4. system keeps checking for that specific route
-   system bekijkt alle vehicles en kijkt of delay
-   vergelijke met afstand volgende voertuig, of nog kan halen
-'''
+    print("\n")
